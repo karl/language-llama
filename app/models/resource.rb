7433eff6e7fb8ed9f38f5029ab70367cdca3c747
Karl O'Keeffe <github@monket.net>
2010-08-25 22:41:36 +0100

    Use Amazon S3

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,7 +3,14 @@ class Resource < ActiveRecord::Base
   belongs_to :language
 
   # Paperclip
-  has_attached_file :document
+  has_attached_file :document,
+    :storage => :s3, 
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    }, 
+    :bucket => ENV['S3_BUCKET'],
+    :path => "/:style/:filename"
 
   validates_attachment_presence :document
         
